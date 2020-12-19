104c234a69956548feb2b724b990372e1c5ffd37
Richard Katsnelson <rkatsnelson91@gmail.com>
2020-12-19 09:58:34 -0500

    clean up and finish 🧹 🏁

---
+++ employeedirectory/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import DataArea from "./DataArea";
+import "../styles/Main.css";
+
+function Main() {
+    return ( <
+        >
+        <
+        DataArea / >
+        <
+        />
+    );
+};
+
+export default Main;
